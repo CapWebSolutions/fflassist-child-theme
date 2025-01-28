f495ae530cf282ff0fbadefe108b00fb5cb51cf6
Matt Ryan <msr1222@gmail.com>
2025-01-28 10:32:34 -0500

    Adjust child theme structure to align with Kadence Child recommendations

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.1.1] - 2025-01-20
+
+### Removed
+
+- All debug code & commented out stuff. 
+
+## [1.1.0] - 2024-11-12
+
+### Changed
+
+- Rebuilt using Kadence child theme generator. 
+
+## [1.0.0] - 2014-11-14
+
+### Added
+
+- Initial commit
--- functions.php
+++ functions.php
@@ -1,19 +1,7 @@
 <?php
 /*
- * This is the child theme for Kadence theme, generated with Generate Child Theme plugin by catchthemes.
+ * This is the child theme for Kadence theme
  *
- * (Please see https://developer.wordpress.org/themes/advanced-topics/child-themes/#how-to-create-a-child-theme)
- */
-// add_action( 'wp_enqueue_scripts', 'fflassist_child_theme_enqueue_styles' );
-// function fflassist_child_theme_enqueue_styles() {
-//     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-//     wp_enqueue_style( 'child-style',
-//         get_stylesheet_directory_uri() . '/style.css',
-//         array('parent-style')
-//     );
-// }
-/*
- * Your code goes below
  */
 
  /**
@@ -49,25 +37,3 @@
      return wp_parse_args( $new_defaults, $defaults );
  }
  add_filter( 'kadence_theme_options_defaults', 'fflassist_child_change_option_defaults', 20 );
-
-function custom_nav_menu_items($items, $args) {
-    if ($args->theme_location == 'primary') {
-        if (is_user_logged_in()) {
-            $loginoutlink = '<li><a class="loginout" href="' . wp_logout_url() . '">Log Out</a></li>';
-        } else {
-            $loginoutlink = '<li><a class="loginout" href="' . wp_login_url() . '">Log In</a></li>';
-        }
-        $items .= $loginoutlink;
-    }
-    error_log( print_r( (object)
-    [
-        'file' => __FILE__,
-        'method' => __METHOD__,
-        'line' => __LINE__,
-        'dump' => [
-            $items,
-        ],
-    ], true ) );
-    return $items;
-}
-// add_filter('wp_nav_menu_items', 'custom_nav_menu_items', 10, 2);
